8c827009c54a7d3b185ba9f5239bb5bfb7a3007e
rajankit3107 <e22cseu0331@bennett.edu.in>
2025-08-21 17:01:16 +0530

    setup userRouter and todoRouter

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,9 @@
+import { todoRouter } from "../routers/todo";
+import { userRouter } from "../routers/user";
 import { router } from "./trpc";
 const appRouter = router({
-  // ...
+  user: userRouter,
+  todo: todoRouter,
 });
 // Export type router type signature,
 // NOT the router itself.
--- server/trpc.ts
+++ server/trpc.ts
@@ -1,9 +1,13 @@
 import { initTRPC } from "@trpc/server";
+import { Todo, User } from "./db";
 /**
  * Initialization of tRPC backend
  * Should be done only once per backend!
  */
-const t = initTRPC.context<{ userId?: string }>().create();
+const t = initTRPC
+  .context<{ db: { Todo: typeof Todo; User: typeof User }; userId?: string }>()
+  //we put the context of database to make the testing easier
+  .create();
 /**
  * Export reusable router and procedure helpers
  * that can be used throughout the router
